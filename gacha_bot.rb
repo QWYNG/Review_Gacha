7e42f9e61f3c6d782ed60280a14f92fd6949320d
QWYNG <ikusawasi@gmail.com>
2019-02-27 21:35:56 +0900

    order

--- gacha_bot.rb
+++ gacha_bot.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 require 'json'
 require './reviewer_consts/reviewer_consts'
 
+def reviewer_gacha
+  essntial_reviewer_id = ESSENTIAL_REVIEWERS_IDS.gacha_doesnt_contain(data.user)
+  reviewer_id = OTHER_REVIEWERS_IDS.gacha_doesnt_contain(data.user)
+  "<@#{essntial_reviewer_id}><@#{reviewer_id}>レビューお願いします"
+end
+
 post '/' do
   # Might want to add a token read from env variable
   #status 500 unless request['token'] == 'your-token-here'
@@ -13,10 +19,4 @@ post '/' do
     title: 'GachaBot',
     text: text,
   }.to_json
-
-  def reviewer_gacha
-    essntial_reviewer_id = ESSENTIAL_REVIEWERS_IDS.gacha_doesnt_contain(data.user)
-    reviewer_id = OTHER_REVIEWERS_IDS.gacha_doesnt_contain(data.user)
-    "<@#{essntial_reviewer_id}><@#{reviewer_id}>レビューお願いします"
-  end
 end
