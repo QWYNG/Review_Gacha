6fa15fd1b291329553f49796131826a17ea26fee
QWYNG <ikusawasi@gmail.com>
2019-02-27 22:45:14 +0900

    rubocop

--- gacha_bot.rb
+++ gacha_bot.rb
@@ -24,7 +24,7 @@ end
 post '/set_reviewer' do
   status 500 if request['token'] != ENV['VERIFICATION_TOKEN']
 
-  if request['text'].include("必須")
+  if request['text'].include('必須')
     ESSENTIAL_REVIEWERS_IDS << request['user_id']
   else
     OTHER_REVIEWERS_IDS << request['user_id']
