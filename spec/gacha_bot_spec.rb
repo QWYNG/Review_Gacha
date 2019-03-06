b5f2ebfdce839d9dd5a97da2d87bff7fd0e9c757
QWYNG <ikusawasi@gmail.com>
2019-03-06 21:45:26 +0900

    add spec

--- spec/gacha_bot_spec.rb
+++ spec/gacha_bot_spec.rb
@@ -3,23 +3,30 @@ ENV['APP_ENV'] = 'test'
 require './gacha_bot'
 require 'rspec'
 require 'rack/test'
+require './array_extension.rb'
 
 describe "gacha_bot" do
-  let(:reviewer_box) { ReviewerBox.new(essential_reviewers: ['THERESH', 'ALISTAR'], other_reviewrs: ['JANNA', 'MORGANA'])}
+  let!(:reviewer_box) { ReviewerBox.new(essential_reviewers: ['THERESH', 'ALISTAR'], other_reviewrs: ['JANNA', 'MORGANA'])}
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
   end
 
-  context "post / " do
+  describe 'post / ' do
     before do
       post '/'
     end
-    
-    it 'responce gacha' do
+
+    it 'responce ok' do
       expect(last_response).to be_ok
     end
   end
-  
+
+  describe 'post /reviewer' do
+    it 'return reviewer json' do
+      post '/reviewer'
+      expect(last_response).to be_ok
+    end
+  end
 end
